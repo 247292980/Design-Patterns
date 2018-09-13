c4ce9efc7732ad3eb3bbdfaeb9c0576cd71f8eb5
liangguiping <liangguiping@tuandai.com>
2018-09-13 11:36:59 +0800

    Java与模式-多例模式有上限/无上限

---
+++ designpatterns/src/main/java/com/lgp/designpatterns/multition/DieMultiton.java
@@ -0,0 +1,43 @@
+package com.lgp.designpatterns.multition;
+
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * @AUTHOR lgp
+ * @DATE 2018/9/12 17:46
+ * @DESCRIPTION 多例模式
+ * 普通多例模式 定义生成，有数量限制
+ **/
+public class DieMultiton {
+    private static DieMultiton one = new DieMultiton();
+    private static DieMultiton two = new DieMultiton();
+
+    private DieMultiton() {
+    }
+
+    public static DieMultiton getInstance(int which) {
+        if (which == 1) {
+            return one;
+        }
+        return two;
+    }
+
+    synchronized public int dice() {
+        Date now = new Date();
+        Random random = new Random(now.getTime());
+
+        int value = random.nextInt();
+        value = Math.abs(value);
+        value = value % 6;
+        value += 1;
+        return value;
+    }
+
+    public static void main(String[] args) {
+        DieMultiton one = DieMultiton.getInstance(1);
+        DieMultiton two = DieMultiton.getInstance(2);
+        System.out.println(one.dice());
+        System.out.println(two.dice());
+    }
+}
